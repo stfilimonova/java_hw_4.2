b4e41d963ce0556ffb8cae348605dcd2a060c7b5
stfilimonova <stfilimonova@mail.ru>
2023-03-18 06:11:07 +0300

    fixed #1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,10 @@ public class Main {
     long amount = 1000_60;
     boolean registered = true;
 
-    service.calculate(amount, registered);
+    int bonus;
+    bonus = (int) service.calculate(amount, registered);
+
+    System.out.println(bonus);
   }
 }
 
